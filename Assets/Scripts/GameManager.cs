14cecfabcaed57e7b1f9158d1bb5930513833116
mmontgomerytecsup <jneyra@tecsup.edu.pe>
2025-09-16 14:34:28 -0500

    Cambio realizado el 16/09

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int variable;
     void Start()
     {
         
