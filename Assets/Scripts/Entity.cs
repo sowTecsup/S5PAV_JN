2e5bc264c3eee5411ff8fcbe620d084d20fa6b84
mmontgomerytecsup <jneyra@tecsup.edu.pe>
2025-09-16 17:25:42 -0500

    Entity Get Damage

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Entity
+{
+    public int health;
+
+    //-> constructor
+
+    public void GetDamage(int x)
+    {
+
+    }
+    public void GetDamage(int x, float crit)
+    {
+
+    }
+    public void GetDamage(int x, float crit, bool knockback)
+    {
+
+    }
+}
